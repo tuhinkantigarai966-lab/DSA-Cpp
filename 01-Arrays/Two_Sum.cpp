a6076dcd52ec2629cbfb42417f3191ced729e905
TUHIN KANTI GARAI <tuhinkantigarai966@gmail.com>
2026-08-29 23:08:38 +0530

    Add twoSum function to find indices of target sum
    Implemented a brute-force approach to find two indices in the array that sum up to the target value.

--- 01-Arrays/Two_Sum.cpp
+++ 01-Arrays/Two_Sum.cpp
@@ -3,15 +3,22 @@ using namespace std;
 
 class Solution {
 public:
+    // Find two indices whose values add up to target
     vector<int> twoSum(vector<int>& nums, int target) {
+
+        // Compare every pair of elements
         for (int i = 0; i < nums.size(); i++) {
+
             for (int j = i + 1; j < nums.size(); j++) {
+
+                // Check if the current pair gives the target
                 if (nums[i] + nums[j] == target) {
                     return {i, j};
                 }
             }
         }
 
+        // Return empty vector if no pair is found
         return {};
     }
 };

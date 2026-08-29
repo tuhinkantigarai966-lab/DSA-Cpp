eaec9d49eb46eef920071a9bdd54958aed41948c
TUHIN KANTI GARAI <tuhinkantigarai966@gmail.com>
2026-08-30 00:16:15 +0530

    Add Selection Sort implementation in C++
    Implemented the selection sort algorithm to sort an array of integers in-place.

---
+++ 03-Sorting/Selection_Sort.cpp
@@ -0,0 +1,42 @@
+/*
+    Selection Sort
+
+    Approach:
+    Find the smallest element in the unsorted part
+    of the array and place it at the beginning.
+
+    Repeat this process until the entire array
+    is sorted.
+
+    Time Complexity:
+    Best Case: O(n^2)
+    Average Case: O(n^2)
+    Worst Case: O(n^2)
+
+    Space Complexity: O(1)
+*/
+
+#include <vector>
+#include <utility>
+using namespace std;
+
+void selectionSort(vector<int>& nums) {
+
+    int n = nums.size();
+
+    for (int i = 0; i < n - 1; i++) {
+
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; j++) {
+
+            if (nums[j] < nums[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex != i) {
+            swap(nums[i], nums[minIndex]);
+        }
+    }
+}
